42b6195a7acae6ad3eab17aeb27e51809aebae93
Mukesh Kumar <mukeshk0326@gmail.com>
2022-01-09 10:02:01 +0530

    Changes done

---
+++ SingleAccordion/src/components/Data.js
@@ -0,0 +1,14 @@
+export const Data = [
+  {
+    question: 'What is this assignment called?',
+    answer: 'Single Open Accordian'
+  },
+  {
+    question: 'Who is Neeraj Chopra?',
+    answer: 'An Indian Olympic winner'
+  },
+  {
+    question: 'What do you like most Data-structure Front-end or Back-end ',
+    answer: 'Data-structure'
+  }
+];
